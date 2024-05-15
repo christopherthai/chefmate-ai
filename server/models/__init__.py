7b919938935c023088e925fbc76effaaa407beba
Christopher Thai <christopherthai.nyli@gmail.com>
2024-05-15 19:05:38 -0400

    fix: Fix typo in __init__.py

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,5 +1,5 @@
 from .users import User
-from .recipe import Recipe
+from .recipes import Recipe
 from .ingredients import Ingredient
 from .recipe_ingredients import RecipeIngredients
 from .saved_recipes import SavedRecipes
