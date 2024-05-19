1d2fe81426df601ab08c3a2783728b0f3ecd0cbb
Christopher Thai <christopherthai.nyli@gmail.com>
2024-05-19 11:29:19 -0400

    feat: Add login prompt feature

---
+++ client/src/components/LoginPrompt.jsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import { Container, Typography, Button } from "@mui/material";
+
+/**
+ * Page component for the login prompt page
+ * @component
+ * @returns {JSX.Element} JSX.Element
+ */
+function LoginPrompt() {
+  const navigate = useNavigate(); // Hook from react-router-dom to navigate to different pages
+
+  /**
+   * Redirects the user to the login page
+   * @returns {void}
+   */
+  const handleGoLogin = () => {
+    navigate("/login");
+  };
+  return (
+    <Container
+      sx={{
+        textAlign: "center",
+        padding: "0rem",
+        marginTop: "16rem",
+        paddingBottom: "40rem",
+      }}
+    >
+      <Typography variant="h4" component="h1" gutterBottom>
+        Enjoy creating new recipe with us!
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Log in to access this page so you can create your own recipe.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        sx={{ marginTop: "20px" }}
+        onClick={handleGoLogin}
+      >
+        Go to Login Page
+      </Button>
+    </Container>
+  );
+}
+
+export default LoginPrompt;
--- client/src/pages/CreateRecipeForm.jsx
+++ client/src/pages/CreateRecipeForm.jsx
@@ -1,5 +1,19 @@
+import { useSelector } from "react-redux";
+import LoginPrompt from "../components/LoginPrompt";
+
 function CreateRecipeForm() {
-  return <div>CreateRecipeForm</div>;
+  const { isLoggedIn } = useSelector((state) => state.user);
+  return (
+    <>
+      {isLoggedIn ? (
+        <div>
+          <h1>Create Recipe Form</h1>
+        </div>
+      ) : (
+        <LoginPrompt />
+      )}
+    </>
+  );
 }
 
 export default CreateRecipeForm;
